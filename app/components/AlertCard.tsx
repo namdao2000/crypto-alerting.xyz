82853df88b4f8456836c59e9e99664d6b7a026d3
Nam <namdao2000@gmail.com>
2022-07-12 01:38:06 +1000

    feat: removed unused files

--- app/components/AlertCard.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from './AlertCard.module.css';
-import { AlertBox } from './AlertBox';
-
-export const AlertCard: React.FC<any> = ({ alert }) => {
-  return <div className={styles.AlertCard}>{alert.text}</div>;
-};
-
-export default AlertCard;
