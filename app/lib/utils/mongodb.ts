2ff03aeb5c8327f8c91128c6d052bb1a0e846506
Nam <57166096+namdao2000@users.noreply.github.com>
2022-07-12 20:55:03 +1000

    Update mongodb.ts

--- app/lib/utils/mongodb.ts
+++ app/lib/utils/mongodb.ts
@@ -15,7 +15,11 @@ if (process.env.NODE_ENV === 'development') {
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri);
-    global._mongoClientPromise = client.connect();
+    global._mongoClientPromise = client.connect({
+      passphrase: 'admin',
+      user: 'admin',
+      database: 'admin',
+    });  
   }
   clientPromise = global._mongoClientPromise;
 } else {
