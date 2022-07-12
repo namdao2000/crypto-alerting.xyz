d830b1a5fcbac494902fdcbc0d8536a6c5fdb9aa
Nam <57166096+namdao2000@users.noreply.github.com>
2022-07-12 21:47:30 +1000

    Update mongodb.ts

--- app/lib/utils/mongodb.ts
+++ app/lib/utils/mongodb.ts
@@ -15,21 +15,13 @@ if (process.env.NODE_ENV === 'development') {
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri);
-    global._mongoClientPromise = client.connect({
-      passphrase: 'admin',
-      user: 'admin',
-      database: 'admin',
-    });  
+    global._mongoClientPromise = client.connect();  
   }
   clientPromise = global._mongoClientPromise;
 } else {
   // In production mode, it's best to not use a global variable.
   client = new MongoClient(uri);
-  clientPromise = client.connect({
-      passphrase: 'admin',
-      user: 'admin',
-      database: 'admin',
-    });
+  clientPromise = client.connect();
 }
 
 // Export a module-scoped MongoClient promise. By doing this in a
