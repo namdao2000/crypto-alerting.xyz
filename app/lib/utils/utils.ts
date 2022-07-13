0040bb9f0977339f50795f3dc9ae74faa4462342
jrmyok <jrmyok>
2022-07-13 20:07:30 +1000

    feat: added _SERVER env to differentiate local host and prod for app

--- app/lib/utils/utils.ts
+++ app/lib/utils/utils.ts
@@ -1,3 +1,5 @@
-export const server = process.env.SERVER || 'http://localhost:3000';
+export const server = process.env._SERVER || 'http://localhost:3000';
+
+
 
 export const fetcher = (url) => fetch(url).then((r) => r.json());
