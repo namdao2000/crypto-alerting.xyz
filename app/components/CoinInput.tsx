f70f85cb6a07c324bcf00dd5d7c2038fa5d3e8d4
Nam <namdao2000@gmail.com>
2022-07-14 20:24:24 +1000

    feat: minor tweak

--- app/components/CoinInput.tsx
+++ app/components/CoinInput.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Form } from 'react-bootstrap';
 import { Controller } from 'react-hook-form';
 import AsyncSelect from 'react-select/async';
